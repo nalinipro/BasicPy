e928b40d38e078358aa9972a4d6b2b7f5f423b26
rnalini <rnalini@juniper.net>
2024-03-17 04:30:59 +0530

    1st file

---
+++ argv.py
@@ -0,0 +1,9 @@
+#import sys
+
+#for x in sys.argv:
+#	print(x)
+
+# this is the first comment
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
